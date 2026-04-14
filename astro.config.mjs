541710355708d1a47d69b8d8cb527e6f2e169cdc
Naoto Akiyama <n4to4k@gmail.com>
2026-04-15 07:51:30 +0900

    gis

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,6 +14,7 @@ export default defineConfig({
 				{ label: 'GISの全体像', slug: '' },
 				{ label: 'データフォーマット一覧', slug: 'data-formats' },
 				{ label: 'CityGML', slug: 'data-formats/citygml' },
+				{ label: 'glTF', slug: 'data-formats/gltf' },
 				{ label: 'CityGML 3.0', slug: 'data-formats/citygml-3-0' },
 				{ label: 'CityGML 3.0 Conceptual Model Standard', slug: 'data-formats/citygml-3-0/conceptual-model-standard' },
 				{ label: 'CityGML 3.0 GML Encoding Standard', slug: 'data-formats/citygml-3-0/gml-encoding-standard' },
